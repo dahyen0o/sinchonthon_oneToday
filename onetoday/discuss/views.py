d20fa42aba247d63f19ee5226497f0df8d51e405
aqswa <69039161+aqswa@users.noreply.github.com>
2022-08-27 23:35:42 +0900

    Feat: 클래스 추천

--- onetoday/discuss/views.py
+++ onetoday/discuss/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
+
+from recruit.models import RecruitPost
+from myclass.models import Class
 
 # Create your views here.
+
+def recommend(request, pk):
+    group = get_object_or_404(RecruitPost, pk=pk)
+    start_date = group.start_date
+    finish_date = group.finish_date
+    classes = Class.objects.filter(type=group.category, place=group.place).filter(date_range=[start_date, finish_date])
+
+    return render(request, 'hyunyoung.html', {'classes' : classes})
+
--- onetoday/onetoday/urls.py
+++ onetoday/onetoday/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from discuss.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('discuss/<int:pk>', recommend, name='recommend'),
 ]
