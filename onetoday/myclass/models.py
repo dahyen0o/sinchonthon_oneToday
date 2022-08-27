d12e907cfaf16f17a87a3840892ed7e481ea72a3
jongmee <ra123221@gmail.com>
2022-08-27 23:15:48 +0900

    user model update

--- onetoday/myclass/models.py
+++ onetoday/myclass/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from datetime import date
 
 class Class(models.Model):
-    type = models.CharField()
+    type = models.CharField(max_length=200)
     date = models.DateField(default=date.today)
-    place = models.CharField()
-    name = models.CharField()
+    place = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
