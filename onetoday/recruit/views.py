4339551e30e9bd5512609a763ea2c7a6a4e99be5
sen <64001680+dahyen0o@users.noreply.github.com>
2022-08-28 05:28:47 +0900

    add search

--- onetoday/recruit/views.py
+++ onetoday/recruit/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
 from .models import *
+from django.db.models import Q
 
 # Create your views here.
 def mainlist(request):
@@ -43,3 +44,19 @@ def detail(request, post_id):
     # 해당 게시글 출력
     else:
         return render(request, 'detail.html', {'detail': get_object_or_404(RecruitPost, pk=post_id)})
+
+def search(request):
+    if request.method == 'POST':
+        place_list = request.GET.getlist('place_list')
+        category_list = request.GET.getlist('category_list')
+
+        query = Q()
+        for i, place in enumerate(place_list):
+            query |= Q(place=place)
+
+        for i, category in enumerate(category_list):
+            query |= Q(category=category)
+
+        result = RecruitPost.objects.filter(query)
+
+        return render(request, '', {'result': result})
