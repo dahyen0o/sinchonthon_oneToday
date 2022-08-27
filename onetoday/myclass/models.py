94e2e91c57f6a6a7f51f4e5d9d360734f2644d76
jongmee <ra123221@gmail.com>
2022-08-27 22:17:07 +0900

    myclass model

--- onetoday/myclass/models.py
+++ onetoday/myclass/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Class(models.Model):
+    type = models.CharField()
+    date = models.DateField(default=date.today)
+    place = models.CharField()
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
