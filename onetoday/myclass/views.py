10c7d1aa770ef597693a8a620e60411689b7e5e7
aqswa <69039161+aqswa@users.noreply.github.com>
2022-08-28 03:18:44 +0900

    Feat: 참여 중인 모임 보기

--- onetoday/myclass/views.py
+++ onetoday/myclass/views.py
@@ -7,4 +7,8 @@ from django.contrib.auth import get_user_model
 def myclass(request):
     User = get_user_model()
     groups = RecruitPost.objects.filter(user=request.user)
+    for post in RecruitPost.objects.all():
+        participants = post.participants.all()
+        if request.user in participants and post not in groups:
+            groups.add(post)
     return render(request,'myclass.html', {'groups': groups})
