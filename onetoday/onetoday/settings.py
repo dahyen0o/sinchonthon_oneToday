9030670d026b90fbbd200e298cc33dda00038e86
sen <64001680+dahyen0o@users.noreply.github.com>
2022-08-27 21:38:05 +0900

    add apps to setting

--- onetoday/onetoday/settings.py
+++ onetoday/onetoday/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'discuss',
+    'myclass',
+    'recruit',
 ]
 
 MIDDLEWARE = [
