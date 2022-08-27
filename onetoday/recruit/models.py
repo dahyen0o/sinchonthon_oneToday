3b707f07c1097dfee718259fa45b08e637c858f2
sen <64001680+dahyen0o@users.noreply.github.com>
2022-08-28 00:13:09 +0900

    modify model and gitignore

--- onetoday/recruit/models.py
+++ onetoday/recruit/models.py
@@ -3,13 +3,26 @@ from account.models import CustomUser
 
 # Create your models here.
 class RecruitPost(models.Model):
+    place_list = {
+        ('mapo-gu', '마포구'),
+        ('seodaemoon-gu', '서대문구'),
+        ('anyang-si', '안양시')
+    }
+    category_list = {
+        ('perfume', '향수'),
+        ('leather', '가죽'),
+        ('climbing', '클라이밍'),
+        ('ceramic', '도자기'),
+        ('soap', '비누')
+    }
+
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField(blank=True, null=True)
     start_date = models.DateField()
     finish_date = models.DateField()
-    place = models.CharField(max_length=15)
-    category = models.CharField(max_length=15)
+    place = models.CharField(max_length=15, choices=place_list)
+    category = models.CharField(max_length=15, choices=category_list)
     total_people = models.IntegerField()
     curr_people = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
